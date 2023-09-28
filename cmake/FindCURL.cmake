5929a0c23f53f4159dd0bfa8d7e47d94a54591ae
AzanovAA <azanov@bk.ru>
2023-09-28 19:08:49 +0700

    Delete cmake/FindCURL.cmake

--- cmake/FindCURL.cmake
+++
@@ -1,9 +0,0 @@
-include_guard(GLOBAL)
-
-set(CURL_ENABLE_EXPORT_TARGET OFF)
-
-cmodule_add(
-  curl 8.2.1
-  URL "https://github.com/curl/curl/releases/download/curl-8_2_1/curl-8.2.1.tar.xz"
-  URL_HASH SHA256=dd322f6bd0a20e6cebdfd388f69e98c3d183bed792cf4713c8a7ef498cba4894
-)
